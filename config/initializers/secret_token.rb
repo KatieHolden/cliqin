b7651ce17dbb34162601da53cf0ea8b09997e0bd
Katie <katie.holden5@gmail.com>
2012-10-21 17:46:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cliqin::Application.config.secret_token = 'f8268891866d680f36e32d955e681cb7a8d34897603f3a6fb677268d65019c3d8d96d1b50a24c8bbded81b8c9400517c8e0a2b77b4e262c0dddc693bc1bdf4c1'
